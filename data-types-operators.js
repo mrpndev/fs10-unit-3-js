04041e054615b873e2b0116d3694a05687b40870
Paul Niemczyk <mrpndev@gmail.com>
2026-02-24 19:42:16 -0500

    data types complete

--- data-types-operators.js
+++ data-types-operators.js
@@ -67,3 +67,52 @@ console.log(fullName) // p is still there, not "stolen", because immutable
 */
 
 console.log(fullName.length)
+console.log(fullName.toUpperCase())
+console.log(fullName.slice(0, 4))
+
+// ? Can I check what data type something is?
+// ! YES! Using typeof operator
+
+console.log(typeof fullName) // return string
+
+/* 
+	? Numbers
+	* any integer, float, decimal, bla bla numbers
+*/
+
+let age = 25
+let bac = 0.08
+console.log(age)
+console.log(typeof age)
+console.log(age + 10)
+console.log(bac)
+
+/* 
+	? Boolean
+	* binary value (on or off, yes or no)
+	* 1/0
+	* true false
+	* JS has several values which are falsey
+		* 0
+		* false
+		* undefined
+		* null
+		* "" (empty string)
+*/
+
+let isBusy = true
+console.log(isBusy)
+
+console.log(Boolean(0)) // Boolean() allows me to pass a value to check if it is
+
+console.log(Boolean(1))
+console.log(Boolean(""))
+console.log(Boolean(null))
+console.log(Boolean(undefined))
+
+/* 
+	? Null or Undefined
+	* null - nothing
+	* undefined - we haven't got a freakin' clue, can be anything
+*/
+
