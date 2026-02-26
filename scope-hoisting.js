e6b9020bae0036cd0d35d3bb2b0ba0b9616d8194
Paul Niemczyk <mrpndev@gmail.com>
2026-02-25 20:33:08 -0500

    Functions

---
+++ scope-hoisting.js
@@ -0,0 +1,20 @@
+/* 
+	? Scope & Hoisting
+	* JS runs left to right, up and down
+	* but that's only when we learn about it
+
+	? Scope
+	* blocks of code which have different access rights
+	* scope is { }
+	* five types
+		* global
+		* block
+		* function
+		* module
+		* lexical
+*/
+
+// ? Global scope
+
+let welcomeToGlobalScope = "This variable can be accessed in any scope"
+console.log(welcomeToGlobalScope)
